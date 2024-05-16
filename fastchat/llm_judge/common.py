50075bfbaef7db1757cfb2a8ef02494d0034ddf6
Jue Wang <jue@abe95083-04.cloud.together.ai>
2024-05-16 13:15:39 -0700

    bump up openai version

--- fastchat/llm_judge/common.py
+++ fastchat/llm_judge/common.py
@@ -408,20 +408,24 @@ def chat_completion_openai(model, conv, temperature, max_tokens, api_dict=None):
     if api_dict is not None:
         openai.api_base = api_dict["api_base"]
         openai.api_key = api_dict["api_key"]
+        client = openai.OpenAI(api_key = api_dict["api_key"])
+    else:
+        client = openai.OpenAI()
+    
     output = API_ERROR_OUTPUT
     for _ in range(API_MAX_RETRY):
         try:
             messages = conv.to_openai_api_messages()
-            response = openai.ChatCompletion.create(
+            response = client.chat.completions.create(
                 model=model,
                 messages=messages,
                 n=1,
                 temperature=temperature,
                 max_tokens=max_tokens,
             )
-            output = response["choices"][0]["message"]["content"]
+            output = response.choices[0].message.content
             break
-        except openai.error.OpenAIError as e:
+        except Exception as e:
             print(type(e), e)
             time.sleep(API_RETRY_SLEEP)
 
@@ -454,12 +458,12 @@ def chat_completion_openai_azure(model, conv, temperature, max_tokens, api_dict=
             )
             output = response["choices"][0]["message"]["content"]
             break
-        except openai.error.OpenAIError as e:
+        except Exception as e:
             print(type(e), e)
             time.sleep(API_RETRY_SLEEP)
-        except openai.error.InvalidRequestError as e:
-            print(type(e), e)
-            break
+        # except openai.error.InvalidRequestError as e:
+        #     print(type(e), e)
+        #     break
         except KeyError:
             print(response)
             break
